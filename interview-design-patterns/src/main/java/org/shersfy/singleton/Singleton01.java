51421bbace9732f31c967e7abb67566e5a15e0af
yuehan1 <yuehan1@lenovo.com>
2018-02-27 17:31:59 +0800

    comment

--- interview-design-patterns/src/main/java/org/shersfy/singleton/Singleton01.java
+++ interview-design-patterns/src/main/java/org/shersfy/singleton/Singleton01.java
@@ -26,6 +26,7 @@ public class Singleton01 {
 	// 2.非 volatile 变量进行读写的时候，每个线程先从内存拷贝变量到CPU缓存中。
 	// 如果计算机有多个CPU，每个线程可能在不同的CPU上被处理，这意味着每个线程可以拷贝到不同的 CPU cache 中。
 	// 而声明变量是 volatile 的，JVM 保证了每次读变量都从内存中读，跳过 CPU cache 这一步。
+	// 理论上一个cpu核对应一个线程，通过超线程技术可以实现一核双线程，如四核八线程CUP
 	private volatile static Singleton01 single;
 	
 	/***
