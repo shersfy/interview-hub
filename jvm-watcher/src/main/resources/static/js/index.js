9fa33aa5475ee288385a11362499e9f9f3fabc7c
shersfy <shersfy@163.com>
2018-04-28 01:04:47 +0800

    echart

--- jvm-watcher/src/main/resources/static/js/index.js
+++ jvm-watcher/src/main/resources/static/js/index.js
@@ -1,3 +1,35 @@
-function show(){
-	alert(123);
-}
+var jvmChart = echarts.init(document.getElementById('jvmChart'));
+var option = {
+		title: {
+			show: false,
+			text: 'jwatcher_memory'
+		},
+		tooltip : {
+			formatter: "{a} <br/>{b} : {c}%"
+		},
+		toolbox: {
+			feature: {
+				saveAsImage: {
+					show: false,
+					type: 'jpg'
+				}
+			},
+			top : 'bottom',
+			itemGap: 20
+		},
+		series: [
+			{
+				name: 'JVM内存状态',
+				type: 'gauge',
+				detail: {formatter:'{value}%'},
+				data: [{value: 50, name: 'Java进程已使用'}]
+			}
+			]
+};
+
+setInterval(function () {
+	option.series[0].data[0].value = (Math.random() * 100).toFixed(2) - 0;
+	jvmChart.setOption(option, true);
+}, 5000);
+
+jvmChart.setOption(option);
