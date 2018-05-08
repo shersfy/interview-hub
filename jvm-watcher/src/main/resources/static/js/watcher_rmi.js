fbb02de622eaaa420dbb8dbea231c2c20d868b1b
shersfy <shersfy@163.com>
2018-05-08 23:36:01 +0800

    url

--- jvm-watcher/src/main/resources/static/js/watcher_rmi.js
+++ jvm-watcher/src/main/resources/static/js/watcher_rmi.js
@@ -1,6 +1,7 @@
 function submitForm(){
 	var url = $('#url').val();
-	if(url == undefined || url.length==0){
+	if(url == undefined || url.trim().length==0){
+		$.messager.alert("提示", "URL不能为空白", "info");
 		return;
 	}
 	$.messager.confirm('确认', '确认创建RMI连接吗?', function(ok){
